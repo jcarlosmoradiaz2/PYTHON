bc5199ed7313dec297c24ae820229a396e629eb2
jcarlosmoradiaz2 <al_jmorad@iessierradelasvillas.org>
2022-04-04 10:15:18 +0200

    Ejemplos primer dia en python

---
+++ ejemplos.py
@@ -0,0 +1,14 @@
+print("Hola python")
+nombre=""
+edad=0
+
+
+nombre= input("Dime tu nombre  ")
+edad= (int) (input("Dime tu edad  "))
+
+print(nombre,"-", edad)
+
+if(edad>10):
+    print(edad + 5)
+else:
+    print("Menor de 10")
